31d584ecb2f08fc419ad3f8dd4d094fda89ce512
iamsubhsingh <149383087+iamsubhsingh@users.noreply.github.com>
2023-10-30 12:51:35 +0000

    variable

---
+++ basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 154655
+let accountEmail = "abc@gmail.com"
+var accountPassword = "54556"
+let accountCity = "Ghaziabad"
+
+//accountId = 10
+/*
+Not use var as vaiable to overcome the scopes
+*/
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
