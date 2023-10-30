a10afafbfdbc05b77af6167a5cc4f196334e5df2
iamsubhsingh <149383087+iamsubhsingh@users.noreply.github.com>
2023-10-30 13:05:59 +0000

    Datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3); // code readability should be there
+
+console.log("Shubham")
+
+let name = "Shubham"
+let age = 28
+let isLoggedIn = false
+let state;
+
+/*  DataType
+
+number => 2 to power 53
+bigint => use for long type of numbers
+strinig => "values of string"
+boolean => true/false
+null => standalon value
+undefined =>
+symbol => unique
+object
+*/ 
+
+console.log(typeof undefined) // undefiened 
+console.log(typeof null) // Object type of value
